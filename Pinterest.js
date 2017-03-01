85f7f84566cb0ad1f5e76d5ff173d82355939a90
Vadiwoo <karuppiv@oregonstate.edu>
2017-02-28 23:10:31 -0800

    Add files via upload

---
+++ Pinterest.js
@@ -0,0 +1,27 @@
+  
+  /*
+Name             : Vadiwoo Karuppiah
+
+Assignment Title : API-How-to Guide
+
+Date              : February,13, 2017 
+*/
+
+  var body = document.getElementsByTagName("body")[0];
+
+  var host = document.getElementsByTagName("results");
+
+
+  var chapter1 = document.createElement("article");
+  
+  var intro1 = "We can discover and save variety of creative ideas from Pinterest.";
+  var intro2 = " Users can upload, save, sort, and manage images—known as pins—and other media content (e.g., videos) through collections known as pinboards.";
+  var intro3 =  " Pinterest acts as a personalized media platform. Users can browse the content of others in their feed."
+  var intro4 = " In this tutorial will show you how to navigate and make use of Pinterest's API using JavaScript.";
+  
+  var chapter1Text = document.createTextNode(intro1 + intro2 + intro3 + intro4 );
+ chapter1.appendChild(chapter1Text);
+  introduction.appendChild(chapter1);
+
+
+   
